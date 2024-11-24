4039f84ad236f68974e732d6e035bc0bd6a1e335
Leonardo-henriqu2 <bernardes.leonardosouza@gmail.com>
2024-11-24 18:56:39 -0300

    final

--- app/src/main/java/com/example/telas2/MainActivity.kt
+++ app/src/main/java/com/example/telas2/MainActivity.kt
@@ -1,20 +1,26 @@
-package com.example.telas2
+package com.example.avaliacaokotlin
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import com.example.avaliacaokotlin.SegundaTela
+import com.example.avaliacaokotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.icSegundaTela.setOnClickListener{
+            val navegarSegundaTela = Intent(this, SegundaTela::class.java)
+
+            startActivity(navegarSegundaTela)
         }
     }
 }
